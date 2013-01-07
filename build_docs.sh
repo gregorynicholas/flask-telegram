04d291597f020fc503d8cfcb35977641e7cb1432
Gregory Nicholas <faction.gregory@gmail.com>
2013-01-06 16:07:05 -0800

    added pull to build_docs.sh script

--- build_docs.sh
+++ build_docs.sh
@@ -10,6 +10,7 @@ cp -rf ./_build/html/* ../
 cd ../
 git add *.html searchindex.js objects.inv .buildinfo _static/ _sources/
 git commit -m "updated docs html build."
+git pull origin gh-pages
 git push origin gh-pages
 
 git checkout -f master
