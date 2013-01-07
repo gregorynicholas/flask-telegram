39664a6ee42a288ed7020afe65efda11dcaf715d
Gregory Nicholas <faction.gregory@gmail.com>
2013-01-06 16:32:47 -0800

    debugged docs build

--- build_docs.sh
+++ build_docs.sh
@@ -2,14 +2,15 @@
 
 cd ./docs
 make html
+cd ../
 
 git checkout gh-pages
 
-cp -rf ./_build/html/* ../
+cp -rf ./docs/_build/html/* ./
 
-cd ../
 git add *.html searchindex.js objects.inv .buildinfo _static/ _sources/
 git commit -m "updated docs html build."
+git pull origin gh-pages
 git push origin gh-pages
 
 git checkout -f master
