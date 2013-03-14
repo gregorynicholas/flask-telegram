c0ea82fa28ff8a3f84f56f6a01f95a6ffb6abbe9
Gregory Nicholas <faction.gregory@gmail.com>
2013-03-14 11:11:58 -0700

    build_docs script updated.

--- build_docs.sh
+++ build_docs.sh
@@ -3,14 +3,17 @@
 cd ./docs
 make html
 cd ../
+touch .nojekyll
 
 git checkout gh-pages
 
 cp -rf ./docs/_build/html/* ./
 
-git add *.html searchindex.js objects.inv .buildinfo _static/ _sources/
+git add .nojekyll *.html searchindex.js objects.inv .buildinfo _static/ _sources/
 git commit -m "updated docs html build."
 git pull origin gh-pages
 git push origin gh-pages
 
 git checkout -f master
+
+rm -rf .nojekyll ./*.html searchindex.js objects.inv _static/ _sources/
